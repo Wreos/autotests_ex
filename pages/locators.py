5c2af222c255ec7b96b589b14e40945d2adc3f94
as.lozhkovoy <as.lozhkovoy@tensor.ru>
2019-07-16 18:01:55 +0700

    refactoring

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 class MainPageLocators(object):
     LOGIN_LINK= (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators:
+class LoginPageLocators(object):
     LOGIN_FORM=(By.CSS_SELECTOR, "#login_link")
     REGISTRATION_FORM=(By.CSS_SELECTOR, "#login_link")
+
+class ProductPageLocators(object):
+    ADD_TO_BASKET=(By.CSS_SELECTOR, '.btn-lg.btn-primary.btn-add-to-basket')
---
+++ test_product_page.py
@@ -0,0 +1,12 @@
+from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
+
+
+def test_add_to_card(browser):
+
+    link="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser,link)
+    page.open()
+    page.add_to_cart()
+    page.solve_quiz_and_get_code()
+
