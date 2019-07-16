182a0508e0b22e1cdc87817a8b4809e154539ac7
as.lozhkovoy <as.lozhkovoy@tensor.ru>
2019-07-16 18:47:17 +0700

    refactoring

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,6 +6,6 @@ class ProductPage(BasePage):
 
     def add_to_cart(self):
         # реализуйте проверку на корректный url адрес
-        linkcart=self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET), "Basket is not found"
+        linkcart=self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         linkcart.click()
 
